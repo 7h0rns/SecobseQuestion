2b9463492536296513aed7c41e1be7700dea8ea3
loner11 <laner11wfl@gmail.com>
2017-01-23 17:46:08 +0800

    modified questions detail page style

--- resources/views/questions/comment.blade.php
+++ resources/views/questions/comment.blade.php
@@ -1,29 +1,42 @@
 @foreach($answer as $as)
-	<div id="answer_list">
-		<h4 id="answer_content">@MarkDown($as->html_content)</h4>
-		<img src="/uploads/avatars/{{ $as->avatar}}" alt="{{ $as->avatar}}" width="32px" height="32px"/>
-		<div id="answer_name"><a href="/profile/{{ $as->answer_name }}">{{$as->answer_name}}</a></div>
-		<div>{{ $as->created_at }}answer</div>
-		<hr>
-		@foreach($as->comments as $co)
-			<div>
-				<div>@MarkDown($co->html_content) <span>--{{ $co->username }}</span>
-					<span>♦{{$co->created_at->diffForHumans()}}</span></div>
+<div class="row question-answers">
+	<div class="col-md-1 col-sm-1">
+		<div class="answer-votes">
+			<a href="#" class="vote-top" data-toggle="tooltip" data-placement="top" title="This answer is useful"><i class="fa fa-caret-up fa-2x" aria-hidden="true"></i></a>
+			<span class="vote-times">10</span>
+			<a href="#" class="vote-bottom" data-toggle="tooltip" data-placement="bottom" title="This answer is not useful"><i class="fa fa-caret-down fa-2x" aria-hidden="true"></i></a>
+		</div>
+	</div>
+	<div class="col-md-8 col-sm-8">
+		<div class="answer-comment">
+			<div class="answer-content">
+				<article id="answer_content">@MarkDown($as->html_content)</article>
+				<div class="answer-user-info">
+					<span class="answered-time">answered&nbsp;{{ $as->created_at }}</span>
+					<img src="/uploads/avatars/{{ $as->avatar}}" alt="{{ $as->avatar}}" width="32" height="32"/>
+					<a href="/profile/{{ $as->answer_name }}">{{$as->answer_name}}</a>
+				</div>
 			</div>
-		@endforeach
-		<div class="comment">
-			<div class="comments">add a comment(Total comments:{{$as->comments->count('id')}})</div>
-			<div class="addComment" style="display: none">
-				<form action="{{ route('comment.store') }}" method="post" class="form-horizontal">
-					{{ csrf_field() }}
-					<input type="hidden" name="commentable_id" value="{{ $as->id }}">
-					<input type="hidden" name="id" value="{{$question->id}}">
-					<input type="hidden" name="commentable_type" value="App\Answer">
-					<textarea name="content" placeholder="填写评论" required></textarea>
-					<button type="submit" class="btn btn-default">Comment</button>
-				</form>
+			@foreach($as->comments as $co)
+				<div class="comment-content">
+					@MarkDown($co->html_content)
+					<span>–{{ $co->username }}&nbsp;&nbsp;{{$co->created_at->diffForHumans()}}</span>
+				</div>
+			@endforeach
+			<div class="comment">
+				<div class="comments">add a comment(Total comments:{{$as->comments->count('id')}})</div>
+				<div class="addComment" style="display: none">
+					<form action="{{ route('comment.store') }}" method="post" class="form-horizontal">
+						{{ csrf_field() }}
+						<input type="hidden" name="commentable_id" value="{{ $as->id }}">
+						<input type="hidden" name="id" value="{{$question->id}}">
+						<input type="hidden" name="commentable_type" value="App\Answer">
+						<textarea name="content" placeholder="填写评论" required></textarea>
+						<button type="submit" class="btn btn-default">Comment</button>
+					</form>
+				</div>
 			</div>
 		</div>
-		<hr>
 	</div>
+</div>
 @endforeach
