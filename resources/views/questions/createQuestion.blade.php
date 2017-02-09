75a0738340d6ac61bc8ee0883de306202929a1cb
Gasbylei <gasbylei@foxmail.com>
2017-02-09 16:36:28 +0800

    Modified createQuestion and show page style

--- resources/views/questions/createQuestion.blade.php
+++ resources/views/questions/createQuestion.blade.php
@@ -3,90 +3,90 @@
 @section('title', '提问')
 
 @section('css')
-	<link href="/css/simplemde.min.css" rel="stylesheet">
-	<link href="/css/select2.min.css" rel="stylesheet" />
-	<link href="/css/select2-bootstrap.min.css" rel="stylesheet" />
+    <link href="/css/simplemde.min.css" rel="stylesheet">
+    <link href="/css/select2.min.css" rel="stylesheet"/>
+    <link href="/css/select2-bootstrap.min.css" rel="stylesheet"/>
 @endsection
 
 @section('content')
-	<div class="row" style="margin-top: 200px;">
-		<div class="col-md-8 col-md-offset-2 create-question">
-			<form action="/questions" method="POST" class="form-horizontal">
-				{{ csrf_field() }}
-				<div class="form-group">
-					<label for="title" class="col-md-2 control-label">标题</label>
-					<div class="col-md-4">
-						<div class="input-group">
-							<input id="title" type="text" class="form-control" name="title" value="{{ old('title') }}"
-							       autofocus>
-							@if ($errors->has('title'))
-							<span class="help-block">
-						        <strong>{{ $errors->first('title') }}</strong>
-						    </span>
-							@endif
-							<span class="input-group-btn">
-			            		<button type="submit" class="btn btn-primary">发布</button>
-			            	</span>
-						</div>
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="tags" class="col-md-2 control-label">标签</label>
-					<div class="col-md-4" >
-						<div class="input-group">
-							<select class="form-control" multiple="multiple" name="tags[]" id="task-list">
-								@foreach($tags as $id=>$name)
-									<option id="tag" value="{{ $id }}">{{$name}}</option>
-								@endforeach
-							</select>
-							@if ($errors->has('tags'))
-								<div class="alert alert-danger">
-									<strong>{{ $errors->first('tags') }}</strong>
-								</div>
-							@endif
-							<span class="input-group-btn">
-								<button type="button" class="btn btn-primary" id="add">添加标签</button>
-							</span>
-						</div>
-					</div>
-					
-				</div>
-				<div class="form-group">
-					<textarea name="mdContent" id="ID"></textarea>
-					@if ($errors->has('mdContent'))
-						<span class="help-block">
-				        <strong>{{ $errors->first('mdContent') }}</strong>
-				    </span>
-					@endif
-				</div>
-			</form>
-		</div>
-		<div id="debug"></div>
-	</div>
-
-	{{--Modal--}}
-	@include('tags.createtag')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 create-question">
+                <div class="panel panel-default">
+                    <div class="panel-heading">发布问题</div>
+                    <div class="panel-body">
+                        <form action="/questions" method="POST">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label for="title">标题</label>
+                                <input id="title" type="text" class="form-control" name="title"
+                                       value="{{ old('title') }}"
+                                       autofocus>
+                                @if ($errors->has('title'))
+                                    <span class="help-block">
+						                <strong>{{ $errors->first('title') }}</strong>
+						            </span>
+                                @endif
+                            </div>
+                            <div class="form-group">
+                                <label for="tags">标签</label>
+                                <div class="input-group">
+                                    <select class="form-control" multiple="multiple" name="tags[]" id="task-list">
+                                        @foreach($tags as $id=>$name)
+                                            <option id="tag" value="{{ $id }}">{{$name}}</option>
+                                        @endforeach
+                                    </select>
+                                    @if ($errors->has('tags'))
+                                        <div class="alert alert-danger">
+                                            <strong>{{ $errors->first('tags') }}</strong>
+                                        </div>
+                                    @endif
+                                    <span class="input-group-btn">
+								        <button type="button" class="btn btn-success" id="add">添加标签</button>
+							        </span>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="mdContent">描述</label>
+                                <textarea name="mdContent" id="ID"></textarea>
+                                @if ($errors->has('mdContent'))
+                                    <span class="help-block">
+				                        <strong>{{ $errors->first('mdContent') }}</strong>
+				                    </span>
+                                @endif
+                            </div>
+                            <button type="submit" class="btn btn-success pull-right">发布问题</button>
+                        </form>
+                    </div>
+                    <div id="debug"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    {{--Modal--}}
+    @include('tags.createtag')
 @endsection
 
 @section('js')
-	<script src="/js/simplemde.min.js"></script>
-	<script src="/js/select2.min.js"></script>
-	<link href="http://cdn.bootcss.com/toastr.js/2.1.3/toastr.min.css" rel="stylesheet">
-	<script src="http://cdn.bootcss.com/toastr.js/2.1.3/toastr.min.js"></script>
-	<script src="/	js/tag.js"></script>
-	<script type="text/javascript">
-		$("select").select2({
-			tokenSeparators: [",", " "],
-			maximumSelectionLength: 5,
-			placeholder: "选择标签",
-			theme: "bootstrap"
-		});
-	</script>
-	<script>
-		var simplemde = new SimpleMDE({
-			element: $("#ID")[0]
-		});
-	</script>
+    <script src="/js/simplemde.min.js"></script>
+    <script src="/js/select2.min.js"></script>
+    <link href="http://cdn.bootcss.com/toastr.js/2.1.3/toastr.min.css" rel="stylesheet">
+    <script src="http://cdn.bootcss.com/toastr.js/2.1.3/toastr.min.js"></script>
+    <script src="/	js/tag.js"></script>
+    <script type="text/javascript">
+        $("select").select2({
+            tokenSeparators: [",", " "],
+            maximumSelectionLength: 5,
+            placeholder: "选择标签",
+            theme: "bootstrap"
+        });
+    </script>
+    <script>
+        var simplemde = new SimpleMDE({
+            element: $("#ID")[0]
+        });
+    </script>
 
 
 @endsection
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -1,177 +1,189 @@
 @extends('layouts.app')
 
 @section('title')
-	{{ $question->title }}
+    {{ $question->title }}
 @endsection
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/other/show.css') }}">
-<link href="{{ asset('css/simplemde.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/other/show.css') }}">
+    <link href="{{ asset('css/simplemde.min.css') }}" rel="stylesheet">
 @endsection('css')
 
 @section('content')
+    <style>
+        .CodeMirror, .CodeMirror-scroll {
+            height: 200px;
+        }
+    </style>
+    <div class="container">
+        <div class="row">
+            @if(Session::has('status'))
+                <div class="alert alert-success">
+                    <button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    {{ Session::get('status') }}
+                </div>
+            @endif
+            <div class="question-header">
+                <span class="title">{{ $question->title }}</span>
+            </div>
+            <div class="col-md-9 col-sm-9">
+                <div class="question-mainbar">
+                    <div class="row">
+                        <div class="col-md-1 col-sm-1">
+                            <div class="question-vote">
+                                <a href="#" class="vote-top" data-toggle="tooltip" data-placement="top"
+                                   title="This question shows research effort; it is useful and clear"><i
+                                            class="fa fa-caret-up fa-2x" aria-hidden="true"></i></a>
+                                <span class="vote-times">1</span>
+                                <a href="#" class="vote-bottom" data-toggle="tooltip" data-placement="bottom"
+                                   title="This question does not show any research effort; it is unclear or not useful"><i
+                                            class="fa fa-caret-down fa-2x" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-md-8 col-sm-8">
+                            <div class="question-content">
+                                <article>
+                                    @MarkDown($question->content)
+                                </article>
 
-	<div class="container">
-		<div class="row">
-			@if(Session::has('status'))
-				<div class="alert alert-success">
-					<button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
-					{{ Session::get('status') }}
-				</div>
-			@endif
-			<div class="question-header">
-				<span class="title">{{ $question->title }}</span>
-			</div>
-			<div class="col-md-9 col-sm-9">
-				<div class="question-mainbar">
-					<div class="row">
-						<div class="col-md-1 col-sm-1">
-							<div class="question-vote">
-								<a href="#" class="vote-top" data-toggle="tooltip" data-placement="top" title="This question shows research effort; it is useful and clear"><i class="fa fa-caret-up fa-2x" aria-hidden="true"></i></a>
-								<span class="vote-times">1</span>
-								<a href="#" class="vote-bottom" data-toggle="tooltip" data-placement="bottom" title="This question does not show any research effort; it is unclear or not useful"><i class="fa fa-caret-down fa-2x" aria-hidden="true"></i></a>
-							</div>
-						</div>
-						<div class="col-md-8 col-sm-8">
-							<div class="question-content">
-								<article>
-									@MarkDown($question->content)
-								</article>
+                                <div class="tags">
+                                    @unless($question->tags->isEmpty())
+                                        @foreach($question->tags as $tag)
+                                            <a href="{{url('tag/'.$tag->id.'')}}">{{ $tag->name }}</a>
+                                        @endforeach
+                                    @endunless
+                                </div>
 
-								<div class="tags">
-									@unless($question->tags->isEmpty())
-											@foreach($question->tags as $tag)
-												<a href="{{url('tag/'.$tag->id.'')}}">{{ $tag->name }}</a>
-											@endforeach
-									@endunless
-								</div>
+                                <div class="user-info">
+                                    <span class="asked-time">{{ $question->created_at }}提问</span>
+                                    <img src="/uploads/avatars/{{ $userAvatar }}" alt="{{ $userAvatar }}" width="32"
+                                         height="32"/>
+                                    <a href="/profile/{{ $question->username }}">{{ $question->username }}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="answers">
+                        <span>{{$count}}个回答</span>
+                    </div>
+                </div>
 
-								<div class="user-info">
-									<span class="asked-time">{{ $question->created_at }}提问</span>
-									<img src="/uploads/avatars/{{ $userAvatar }}" alt="{{ $userAvatar }}" width="32" height="32"/>
-								  <a href="/profile/{{ $question->username }}">{{ $question->username }}</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="answers">
-						<span>{{$count}}个回答</span>
-					</div>
-				</div>
-				@include('questions.comment')
-			</div>
+                @include('questions.comment')
 
-			<div class="col-md-3 col-sm-3">
-				<div class="question-sidebar">
-					<div class="question-state">
-						<p>提问&nbsp;&nbsp;&nbsp;<span>{{ $question->created_at }}</span></p>
-						<p>得票&nbsp;&nbsp;&nbsp;<span>{{ $question->readtimes }}</span></p>
-						<p>回答&nbsp;&nbsp;&nbsp;<span>10</span></p>
-					</div>
-					<div class="related">
-						<h4>Related</h4>
-						<div class="related-list">
-							<div class="list-content">
-								<div class="answer-times">
-									<span>10</span>
-								</div>
-								<a href="#">
-										<span>You can try to make a meaningful question</span>
-								</a>
-							</div>
-							<div class="list-content">
-								<div class="answer-times">
-									<span>10</span>
-								</div>
-								<a href="#">
-										<span>You can try to make a meaningful question</span>
-								</a>
-							</div>
-							<div class="list-content">
-								<div class="answer-times">
-									<span>10</span>
-								</div>
-								<a href="#">
-										<span>You can try to make a meaningful question</span>
-								</a>
-							</div>
-							<div class="list-content">
-								<div class="answer-times">
-									<span>10</span>
-								</div>
-								<a href="#">
-										<span>You can try to make a meaningful question</span>
-								</a>
-							</div>
-							<div class="list-content">
-								<div class="answer-times">
-									<span>10</span>
-								</div>
-								<a href="#">
-										<span>You can try to make a meaningful question</span>
-								</a>
-							</div>
-							<div class="list-content">
-								<div class="answer-times">
-									<span>10</span>
-								</div>
-								<a href="#">
-										<span>You can try to make a meaningful question</span>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                @if (Auth::guest())
+                    <div class="container">
+                        <h3 align="center">登录后可以提交回答
+                            <button type="button" id="tologin" class="btn btn-primary">登录</button>
+                        </h3>
+                    </div>
+                @else
+
+                        <div><h3>撰写回答:</h3></div>
+                        <form id="answer" action="/answer" method="POST" class="form-horizontal">
+                            <input type="hidden" name="question_id" id="question_id" value="{{ $question->id }}">
+                            <textarea name="answer_content" id="answerEditor"></textarea>
+                            {!! csrf_field() !!}
+                            <button type="submit" id="tsave" class="btn btn-lg btn-success pull-right">提交回答</button>
+                        </form>
 
 
+                        @if ($errors->has('mdContent'))
+                            <span class="help-block">
+				                <strong>{{ $errors->first('mdContent') }}</strong>
+			                </span>
+                        @endif
+                @endif
+            </div>
 
-	@if (Auth::guest())
-		<div class="container">
-			<h3 align="center">登录后可以提交回答
-				<button type="button" id="tologin" class="btn btn-primary">登录</button>
-			</h3>
-		</div>
-	@else
-		<div class="container">
-			<div><h3>撰写回答:</h3></div>
-			<form id="answer" action="/answer" method="POST" class="form-horizontal">
-				<input type="hidden" name="question_id" id="question_id" value="{{ $question->id }}">
-				<textarea name="answer_content" id="answerEditor"></textarea>
-				{!! csrf_field() !!}
-				<button type="submit" id="tsave" class="btn btn-primary">提交回答</button>
-			</form>
+            <div class="col-md-3 col-sm-3">
+                <div class="question-sidebar">
+                    <div class="question-state">
+                        <p>提问&nbsp;&nbsp;&nbsp;<span>{{ $question->created_at }}</span></p>
+                        <p>得票&nbsp;&nbsp;&nbsp;<span>{{ $question->readtimes }}</span></p>
+                        <p>回答&nbsp;&nbsp;&nbsp;<span>10</span></p>
+                    </div>
+                    <div class="related">
+                        <h4>Related</h4>
+                        <div class="related-list">
+                            <div class="list-content">
+                                <div class="answer-times">
+                                    <span>10</span>
+                                </div>
+                                <a href="#">
+                                    <span>You can try to make a meaningful question</span>
+                                </a>
+                            </div>
+                            <div class="list-content">
+                                <div class="answer-times">
+                                    <span>10</span>
+                                </div>
+                                <a href="#">
+                                    <span>You can try to make a meaningful question</span>
+                                </a>
+                            </div>
+                            <div class="list-content">
+                                <div class="answer-times">
+                                    <span>10</span>
+                                </div>
+                                <a href="#">
+                                    <span>You can try to make a meaningful question</span>
+                                </a>
+                            </div>
+                            <div class="list-content">
+                                <div class="answer-times">
+                                    <span>10</span>
+                                </div>
+                                <a href="#">
+                                    <span>You can try to make a meaningful question</span>
+                                </a>
+                            </div>
+                            <div class="list-content">
+                                <div class="answer-times">
+                                    <span>10</span>
+                                </div>
+                                <a href="#">
+                                    <span>You can try to make a meaningful question</span>
+                                </a>
+                            </div>
+                            <div class="list-content">
+                                <div class="answer-times">
+                                    <span>10</span>
+                                </div>
+                                <a href="#">
+                                    <span>You can try to make a meaningful question</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-			@if ($errors->has('mdContent'))
-				<span class="help-block">
-				<strong>{{ $errors->first('mdContent') }}</strong>
-			</span>
-			@endif
-		</div>
-	@endif
+
+
 @endsection
 
 @section('js')
-	<script src="/js/simplemde.min.js"></script>
-	<script>
-		$(document).ready(function() {
-			$('#tologin').click(function () {
-				$('#login').modal('show');
-			});
+    <script src="/js/simplemde.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#tologin').click(function () {
+                $('#login').modal('show');
+            });
 
-			$('.comments').click(function() {
-				$comment = $(this).closest('.comment');
-				$comment.siblings().find('.addComment').slideUp();
-				$comment.find('.addComment').fadeToggle(1000, 'swing');
-			});
+            $('.comments').click(function () {
+                $comment = $(this).closest('.comment');
+                $comment.siblings().find('.addComment').slideUp();
+                $comment.find('.addComment').fadeToggle(1000, 'swing');
+            });
 
-			var simplemde = new SimpleMDE({
-				element: $("#answerEditor")[0],
-				codeSyntaxHighlighting: true
-			});
-		});
-	</script>
+            var simplemde = new SimpleMDE({
+                element: $("#answerEditor")[0],
+                codeSyntaxHighlighting: true
+
+            });
+        });
+    </script>
 @endsection
